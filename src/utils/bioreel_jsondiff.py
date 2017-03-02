dff12a036abf1a76965566f1294691f0dc8c93bf
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-03-02 10:45:57 -0800

    Most recent diff for mygene

--- src/utils/bioreel_jsondiff.py
+++ src/utils/bioreel_jsondiff.py
@@ -1,39 +1,69 @@
-STRING_ONLY_KEYS = ['APHIDBASE', 'map_location', 'summary', 'MIM', 'type_of_gene', 'miRBase', 'SGD', 'Araport',
-    'VGNC', 'EcoGene', 'RGD', 'dictyBase', 'VectorBase', 'Xenbase', 'Pathema', 'symbol', 'NASONIABASE',
-    'locus_tag', 'MGI', 'ZFIN', 'TAIR', 'CGNC', 'PseudoCap', 'BGD', 'BEETLEBASE', 'ApiDB_CryptoDB', 'HGNC',
-    'Vega', 'BEEBASE', 'FLYBASE', 'name', 'WormBase', 'AnimalQTLdb']
+"""
 
-DICT_ONLY_KEYS = ['homologene', 'pathway', 'wikipedia', 'uniprot', 'refseq', 'go', 'accession', 'exac', 
-    'reporter', 'reagent']
+    Some code to do a type of diff on mygene object.
 
-LIST_ONLY_KEYS = ['exons_mm9', 'generif', 'exons_hg19', 'exons']
+    Below is the result of a key analysis on all mygene documents circa late 2016-early 2017.
 
-INT_ONLY_KEYS = ['entrezgene', 'taxid']
+    STRING_ONLY_KEYS = ['APHIDBASE', 'map_location', 'summary', 'MIM', 'type_of_gene', 'miRBase', 'SGD', 'Araport',
+        'VGNC', 'EcoGene', 'RGD', 'dictyBase', 'VectorBase', 'Xenbase', 'Pathema', 'symbol', 'NASONIABASE',
+        'locus_tag', 'MGI', 'ZFIN', 'TAIR', 'CGNC', 'PseudoCap', 'BGD', 'BEETLEBASE', 'ApiDB_CryptoDB', 'HGNC',
+        'Vega', 'BEEBASE', 'FLYBASE', 'name', 'WormBase', 'AnimalQTLdb']
 
-STRING_OR_LIST_KEYS = ['pfam', 'pir', 'ipi', 'prosite', 'ec', 'unigene', 'pharmgkb', 'alias', 'pdb', 'other_names']
+    DICT_ONLY_KEYS = ['homologene', 'pathway', 'wikipedia', 'uniprot', 'refseq', 'go', 'accession', 'exac', 
+        'reporter', 'reagent']
 
-INT_OR_LIST_KEYS = ['retired']
+    LIST_ONLY_KEYS = ['exons_mm9', 'generif', 'exons_hg19', 'exons']
 
-DICT_OR_LIST_KEYS = ['genomic_pos', 'genomic_pos_mm9', 'ensembl', 'genomic_pos_hg19', 'interpro']
+    INT_ONLY_KEYS = ['entrezgene', 'taxid']
 
-MYGENE_ROOT_KEYS = ['APHIDBASE', 'AnimalQTLdb', 'ApiDB_CryptoDB', 'Araport', 'BEEBASE', 'BEETLEBASE',
- 'BGD', 'CGNC', 'EcoGene', 'FLYBASE', 'HGNC', 'MGI', 'MIM', 'NASONIABASE', 'Pathema', 'PseudoCap',
- 'RGD', 'SGD', 'TAIR', 'VGNC', 'VectorBase', 'Vega', 'WormBase', 'Xenbase', 'ZFIN', 'accession',
- 'alias', 'dictyBase', 'ec', 'ensembl', 'entrezgene', 'exac', 'exons', 'exons_hg19', 'exons_mm9',
- 'generif', 'genomic_pos', 'genomic_pos_hg19', 'genomic_pos_mm9', 'go', 'homologene', 'interpro',
- 'ipi', 'locus_tag', 'map_location', 'miRBase', 'name', 'other_names', 'pathway', 'pdb', 'pfam',
- 'pharmgkb', 'pir', 'prosite', 'reagent', 'refseq', 'reporter', 'retired', 'summary', 'symbol',
- 'taxid', 'type_of_gene', 'unigene', 'uniprot', 'wikipedia']
+    STRING_OR_LIST_KEYS = ['pfam', 'pir', 'ipi', 'prosite', 'ec', 'unigene', 'pharmgkb', 'alias', 'pdb', 'other_names']
 
+    INT_OR_LIST_KEYS = ['retired']
 
+    DICT_OR_LIST_KEYS = ['genomic_pos', 'genomic_pos_mm9', 'ensembl', 'genomic_pos_hg19', 'interpro']
 
-def process_go(go_src, go_dest):
-    # specifically process the go key
-    
+    MYGENE_ROOT_KEYS = ['APHIDBASE', 'AnimalQTLdb', 'ApiDB_CryptoDB', 'Araport', 'BEEBASE', 'BEETLEBASE',
+     'BGD', 'CGNC', 'EcoGene', 'FLYBASE', 'HGNC', 'MGI', 'MIM', 'NASONIABASE', 'Pathema', 'PseudoCap',
+     'RGD', 'SGD', 'TAIR', 'VGNC', 'VectorBase', 'Vega', 'WormBase', 'Xenbase', 'ZFIN', 'accession',
+     'alias', 'dictyBase', 'ec', 'ensembl', 'entrezgene', 'exac', 'exons', 'exons_hg19', 'exons_mm9',
+     'generif', 'genomic_pos', 'genomic_pos_hg19', 'genomic_pos_mm9', 'go', 'homologene', 'interpro',
+     'ipi', 'locus_tag', 'map_location', 'miRBase', 'name', 'other_names', 'pathway', 'pdb', 'pfam',
+     'pharmgkb', 'pir', 'prosite', 'reagent', 'refseq', 'reporter', 'retired', 'summary', 'symbol',
+     'taxid', 'type_of_gene', 'unigene', 'uniprot', 'wikipedia']
 
-def process_simple_list_diffs(src_list, dest_list, key, diff_id):
+"""
+
+# These are root keys where the value is an entity or list of entities that can be directly diffed
+SIMPLE_ENTITIES = ['APHIDBASE', 'AnimalQTLdb', 'ApiDB_CryptoDB', 'Araport', 
+                'BEEBASE', 'BEETLEBASE', 'BGD', 'CGNC', 'EcoGene', 'FLYBASE', 'HGNC', 'MGI', 'MIM', 
+                'NASONIABASE', 'Pathema', 'PseudoCap', 'RGD', 'SGD', 'TAIR', 'VGNC', 'VectorBase', 'Vega', 
+                'WormBase', 'Xenbase', 'ZFIN', 'alias', 'dictyBase', 'ec', 'entrezgene', 'generif', 'interpro',
+                'locus_tag', 'map_location', 'miRBase', 'name', 'other_names', 'pfam', 'pharmgkb', 'prosite',
+                'retired', 'summary', 'symbol', 'taxid', 'type_of_gene', 'unigene', 'genomic_pos', 
+                'genomic_pos_hg19', 'genomic_pos_mm9', 'pir', 'pdb', 'ipi', 'exons', 'exons_hg19', 'exons_mm9']
+
+# These are root keys where the diffed entity is nested inside a dict.  The value list is the subkey that contains
+# a diff-able entity or entity list
+NESTED_ENTITIES = {'go': ['BP', 'MF', 'CC'], 'accession': ['genomic', 'protein', 'rna', 'translation'], 
+                'ensembl': ['gene', 'protein', 'transcript', 'translation'], 
+                'homologene': ['genes', 'id'], 'refseq': ['genomic', 'protein', 'rna', 'translation'],
+                'uniprot': ['TrEMBL', 'Swiss-Prot'], 'pathway': ['biocarta', 'humancyc', 'kegg', 
+                'mousecyc', 'netpath', 'pharmgkb', 'pid', 'reactome', 'smpdb', 'wikipathways', 'yeastcyc'],
+                'wikipedia': ['url_stub']}
+
+# These are root keys where the keys contain values, and need special processing
+DICT_ITEM_ENTITIES = ['reporter', 'reagent']
+
+# These are root keys with no lists and potentially many nested levels...
+ALL_ITEM_ENTITIES = ['exac']
+
+def process_simple_list_diffs(src, dest, key, diff_id):
     # return ops for list diffs
-    _src = set(src_list); _dest = set(dest_list)
+    if not isinstance(src, list):
+        src = list(src)
+    if not isinstance(dest, list):
+        dest = list(dest)
+    _src = set(src); _dest = set(dest)
     _diffs = []
     for item in _src.difference(_dest):
         _diffs.append({'op': 'remove', 'path': key, 'item': item, 'diff': diff_id})
@@ -41,15 +71,32 @@ def process_simple_list_diffs(src_list, dest_list, key, diff_id):
         _diffs.append({'op': 'add', 'path': key, 'item': item, 'diff': diff_id})
     return _diffs
 
-def dummy(*args, **kwargs):
-    return []
+def process_all_item_entity_diffs(src, dest, key, diff_id):
+    # 
+    _diffs = []
+    if src == dest:
+        return _diffs
+    
+    def _get_dest(dot):
+        fields = dot.split('.')[1:]
+        _ret = dest
+        for field in fields:
+            _ret = _ret[field]
+        return _ret
+
+    def _traverse(d, _path):
+        if isinstance(d, dict):
+            for (k,v) in d.items():
+                _traverse(v, _path + '.' + k)
+        else:
+            _diffs.extend(process_simple_list_diffs(d, _get_dest(d), _path, diff_id))
+
+    _traverse(src, key)
+    return _diffs
 
 def mygene_diff(src, dest, diff_id):
     if src == dest:
         return []
-
-    osrc = copy.deepcopy(src)
-    odest = copy.deepcopy(dest)
 
     diffs = []
     src_keys = set(src.keys()); dest_keys = set(dest.keys())
@@ -57,121 +104,28 @@ def mygene_diff(src, dest, diff_id):
     # do the top level keys in both:
     for key in src_keys.intersection(dest_keys):
         src_val = src[key]; dest_val = dest[key]
-        # These keys are always dictionaries in root level, and often represent separate entities, 
-        # handle them individually
-        if key == 'go':
-            diff_fn = process_go_diffs
-            _src = src_val
-            _dest = dest_val
-        elif key == 'homologene':
-            for subkey in ['genes', 'id']:
-                src_subval = src_val.get(subkey, [])
-                dest_subval = dest_val(subkey, [])
-                if not isinstance(src_subval, list):
-                    src_subval = [src_subval]
-                if not isinstance(dest_subval, list):
-                    dest_subval = [dest_subval]
-                diffs.extend(process_simple_list_diffs(src_subval, dest_subval, key=key+'.'+subkey, diff_id=diff_id))
-            continue
-        elif key == 'pathway':
-            for subkey in ['reactome']:
-                src_subval = src_val.get(subkey, [])
-                dest_subval = dest_val(subkey, [])
-                if not isinstance(src_subval, list):
-                    src_subval = [src_subval]
-                if not isinstance(dest_subval, list):
-                    dest_subval = [dest_subval]
-                diffs.extend(process_simple_list_diffs(src_subval, dest_subval, key=key+'.'+subkey, diff_id=diff_id))
-            continue 
-        elif key == 'wikipedia':
-            for subkey in ['url_stub']:
-                src_subval = src_val.get(subkey, [])
-                dest_subval = dest_val(subkey, [])
-                if not isinstance(src_subval, list):
-                    src_subval = [src_subval]
-                if not isinstance(dest_subval, list):
-                    dest_subval = [dest_subval]
-                diffs.extend(process_simple_list_diffs(src_subval, dest_subval, key=key+'.'+subkey, diff_id=diff_id))
-            continue 
-        elif key == 'uniprot':
-            for subkey in ['TrEMBL', 'Swiss-Prot']:
-                src_subval = src_val.get(subkey, [])
-                dest_subval = dest_val(subkey, [])
-                if not isinstance(src_subval, list):
-                    src_subval = [src_subval]
-                if not isinstance(dest_subval, list):
-                    dest_subval = [dest_subval]
-                diffs.extend(process_simple_list_diffs(src_subval, dest_subval, key=key+'.'+subkey, diff_id=diff_id))
-            continue 
-        elif key == 'refseq':
-            for subkey in ['genomic', 'protein', 'rna', 'translation']:
-                src_subval = src_val.get(subkey, [])
-                dest_subval = dest_val.get(subkey, [])
-                if not isinstance(src_subval, list):
-                    src_subval = [src_subval]
-                if not isinstance(dest_subval, list):
-                    dest_subval = [dest_subval]
-                diffs.extend(process_simple_list_diffs(src_subval, dest_subval, key=key+'.'+subkey, diff_id=diff_id))
-            continue
-                
-        elif key == 'accession':
-            for subkey in ['genomic', 'protein', 'rna', 'translation']:
-                src_subval = src_val.get(subkey, [])
-                dest_subval = dest_val.get(subkey, [])
-                if not isinstance(src_subval, list):
-                    src_subval = [src_subval]
-                if not isinstance(dest_subval, list):
-                    dest_subval = [dest_subval]
-                diffs.extend(process_simple_list_diffs(src_subval, dest_subval, key=key+'.'+subkey, diff_id=diff_id))
-            continue
-        elif key == 'exac':
-            #
-        elif key == 'reporter':
-            # value tests
-        elif key == 'reagent':
-        
-        # These keys are always lists of dictionaries
-        elif key == 'exons_mm9':
+        # Do simple entities
+        if key in SIMPLE_ENTITIES:
+            diffs.extend(process_simple_list_diffs(src_val, dest_val, key, diff_id))
+        elif key in NESTED_ENTITIES:
+            for subkey in NESTED_ENTITIES[key]:
+                sk_src_val = src_val.get(subkey, []); sk_dest_val = dest_val.get(subkey, [])
+                diffs.extend(process_simple_list_diffs(sk_src_val, sk_dest_val, key + '.' + subkey, diff_id))
+        elif key in DICT_ITEM_ENTITIES:
+            src_subkeys = set(list(src_val.keys())); dest_subkeys = set(list(dest_val.keys()))
+            for subkey in src_subkeys.intersection(dest_subkeys):
+                sk_src_val = src_val.get(subkey); sk_dest_val = dest_val.get(subkey)
+                diffs.extend(process_simple_list_diffs(sk_src_val, sk_dest_val, key + '.' + subkey, diff_id))
 
-        elif key == 'exons_hg19':
+            # in src but not in dest (delete)
+            for subkey in src_subkeys.difference(dest_subkeys):
+                diffs.append({'op': 'delete', 'path': key + '.' + subkey, 'item': src_val[subkey], 'diff': diff_id})
 
-        elif key == 'exons':
-
-        elif key == 'generif':
-
-        # These keys are dictionaries (for one value) and lists of dictionaries for multiple values
-DICT_OR_LIST_KEYS = ['genomic_pos', 'genomic_pos_mm9', 'ensembl', 'genomic_pos_hg19', 'interpro']
-        
-        if ((isinstance(src_val, str) and isinstance(dest_val, str)) or 
-            (isinstance(src_val, int) and isinstance(dest_val, int)) or
-            (isinstance(src_val, str) and isinstance(dest_val, list) and len(dest_val) > 0 and isinstance(dest_val[0], str)) or
-            (isinstance(src_val, int) and isinstance(dest_val, list) and len(dest_val) > 0 and isinstance(dest_val[0], int)) or
-            (isinstance(src_val, list) and len(src_val) > 0 and isinstance(src_val[0], int) and isinstance(dest_val, int)) or
-            (isinstance(src_val, list) and len(src_val) > 0 and isinstance(src_val[0], str) and isinstance(dest_val, str)) or
-            (isinstance(src_val, list) and len(src_val) > 0 and isinstance(src_val[0], int) and isinstance(dest_val, list) and len(dest_val) > 0 and isinstance(dest_val[0], int))
-            (isinstance(src_val, list) and len(src_val) > 0 and isinstance(src_val[0], str) and isinstance(dest_val, list) and len(dest_val) > 0 and isinstance(dest_val[0], str))):
-            diff_fn = process_simple_list_diffs
-            if not isinstance(src_val, list):
-                _src = [src_val]
-            if not isinstance(dest_val, list):
-                _dest = [dest_val]
-        elif ((isinstance(src_val, dict) and isinstance(dest_val, dict)) or
-            (isinstance(src_val, dict) and isinstance(dest_val, list) and len(dest_val) > 0 and isinstance(dest_val[0], dict)) or
-            (isinstance(src_val, list) and len(src_val) > 0 and isinstance(src_val[0], dict) and isinstance(dest_val, dict)) or
-            (isinstance(src_val, list) and len(src_val) > 0 and isinstance(src_val[0], dict) and isinstance(dest_val, list) and len(dest_val) > 0 and isinstance(dest_val[0], dict)):
-            else:
-                if 
-                diff_fn = process_entity_list_diffs
-        else:
-            # should never happen....log to file
-            diff_fn = dummy
-            _src = []
-            _dest = []
-
-        if diff_fn and _src and _dest:
-            key_diffs = diff_fn(_src, _dest, key=key, diff_id=diff_id)
-
-        diffs.extend(key_diffs)
+            # in dest but not in src (add)
+            for subkey in dest_subkeys.difference(src_subkeys):
+                diffs.append({'op': 'add', 'path': key + '.' + subkey, 'item': dest_val[subkey], 'diff': diff_id})
+        elif key in ALL_ITEM_ENTITIES:
+            diffs.extend(process_all_item_entity_diffs(src_val, dest_val, key, diff_id))
 
     # do top level keys in src but not in dest (full key delete)
     for key in src_keys.difference(dest_keys):
